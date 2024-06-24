59616e3f87ebf0aecf5193b5fc830e0c2c465564
Trai Ghita <traighita@gmail.com>
2024-06-24 17:30:53 +0100

    add daily_reminder.py file

---
+++ daily_reminder.py
@@ -0,0 +1,25 @@
+
+task = input("Enter your task: ")
+
+priority = input("Priority (high/medium/low): ").lower()
+
+time_bound = input("Is it time-bound? (yes/no): ").lower()
+
+
+reminder = f"Reminder: '{task}' is a {priority} priority task"
+
+match priority:
+    case 'high':
+        reminder += " that requires immediate attention!"
+    case 'medium':
+        reminder += ". Try to complete it soon."
+    case 'low':
+        reminder += ". Consider completing it when you have free time."
+    case _:
+        reminder = f"Invalid priority entered for task: '{task}'"
+
+
+if time_bound == 'yes':
+    reminder = reminder.replace("!", " today!")
+
+print(reminder)
