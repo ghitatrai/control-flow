179ff2b09fd2e75cc86a4e193caf850c1514e809
Trai Ghita <traighita@gmail.com>
2024-06-24 16:52:15 +0100

    add weather_advice.py file

--- weather_advice.py
+++ weather_advice.py
@@ -1,8 +1,10 @@
 weather = input("What's the weather like today? (sunny/rainy/cold): ")
 
 if (weather == "sunny") :
-    print("Wear a t-shirt and sunglasses")
+    print("Wear a t-shirt and sunglasses.")
 elif (weather == "rainy"):
     print("Don’t forget your umbrella and a raincoat.")
+elif (weather == "cold") :
+    print("Make sure to wear a warm coat and a scarf.")
 else :
-    print("Make sure to wear a warm coat and a scarf")
+    print("Sorry, I don't have recommendations for this weather.")
